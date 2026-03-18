7bb8e3c8cfd69bb08e326faaa5f4d602c38b9571
Jowell <jowell@example.com>
2026-03-18 14:25:39 +0800

    Fixed Tailwind scan paths and main import

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -3,14 +3,12 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}", // Added this to find files in the root
   ],
   theme: {
     extend: {
       animation: {
         'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-      },
-      backdropBlur: {
-        xs: '2px',
       },
     },
   },
